6d02493bb68626c3f9ab2c1295e5e4b643465411
aeromeror <aeromeror@unal.edu.co>
2021-11-08 01:24:35 -0500

    yavalderramac

--- content/sketches/troxler.js
+++ content/sketches/troxler.js
@@ -1,11 +1,15 @@
 let img;
 
-
+function preload(){ 
+  img = loadImage('content\sketches\troxler_main.png'
+  );
+  } 
 function setup() {
   
   createCanvas(494, 361);
-  
-  img = loadImage('content\sketches\troxler_main.png'); // Load the image
+  noLoop();
+  //image(img, 0, 0);
+  //img = loadImage('content\sketches\troxler_main.png'); // Load the image
 }
 
 function draw() {
