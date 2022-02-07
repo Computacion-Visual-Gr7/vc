ceee6321dfba911cd3a8028531a3ee4a363fe0e9
caachaparrosi <caachaparrosi@unal.edu.co>
2022-02-06 19:14:51 -0500

    Fotomosaico Software

---
+++ content/docs/shortcodes/workshop3/FotomosaicoSoftware.md
@@ -0,0 +1,3 @@
+# Fotomosaico por software
+{{< p5-iframe2 sketch="/vc/sketches/mosaicosoftware.js" width="750" height="550" >}}
+
---
+++ content/sketches/mosaicosoftware.js
@@ -0,0 +1,60 @@
+let imageSet=[],setLuma=[];
+let WIDTH=500,HEIGHT=500;
+var imgMosaic;
+let scl;
+function preload(){
+  var img;
+  for(var i=1;i<=10;i++){
+    img = loadImage('/vc/sketches/shaders/imagenesmosaico/'+i+'.jpg');
+    imageSet.push(img);
+  }
+  imgMosaic = loadImage("/vc/sketches/shaders/imagenesmosaico/"+int(random(10))+".jpg");
+}
+
+function setup(){
+
+  scl=createSlider(1,16,4);
+  scl.position(600,50);
+  scl.style('width','50px');
+  createCanvas(imgMosaic.width,imgMosaic.height);
+  quadrille = createQuadrille(imageSet);
+  quadrille.sort();
+  for (var j = 0;j<10;j++){
+    var imgLuma = getLuma(quadrille.read(0,j));
+    setLuma.push(imgLuma);
+  }
+}
+
+
+function draw() {
+  mosaicQuadrille = createQuadrille(10*scl.value(),imgMosaic);
+for(var cx=0;cx<10*scl.value();cx++){
+    for(var cy=0;cy<mosaicQuadrille.size/(10*scl.value());cy++){
+       var alum = mosaicQuadrille.read(cy,cx)[0] * 0.299 + mosaicQuadrille.read(cy,cx)[1] * 0.587 + mosaicQuadrille.read(cy,cx)[2] * 0.114;
+      var closest = setLuma.reduce(function(prev, curr) {
+        return (Math.abs(curr - alum) < Math.abs(prev - alum) ? curr : prev);
+      });
+      var cell = setLuma.indexOf(closest);
+      mosaicQuadrille.fill(cy,cx,imageSet[cell]);
+    }
+  }
+  drawQuadrille(mosaicQuadrille, {cellLength: 40 / scl.value(), outlineWeight: 1.6 / scl.value(), outline: color(255)});
+  image(imgMosaic,400,0,imgMosaic.width/10,imgMosaic.height/10);
+}
+
+
+function getLuma(imagelum){
+  imagelum.loadPixels();
+  var avgluma=0;
+  
+  for(var k=0;k<imagelum.pixels.length;k+=4){
+      let r = imagelum.pixels[k+0];
+      let g = imagelum.pixels[k+1];
+      let b = imagelum.pixels[k+2];
+      let a = imagelum.pixels[k+3];
+      var luma = 0.299 * r + 0.587 * g + 0.114 * b;
+      avgluma +=luma;
+    }
+  avgluma = avgluma/(imagelum.pixels.length/4);
+  return avgluma;
+}
