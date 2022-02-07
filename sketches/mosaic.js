08a5c5735f361eb12af998952075ce5e9876d68c
aeromeror <aeromeror@users.noreply.github.com>
2022-02-07 00:22:28 +0000

    deploy: 5470fa48d0a118ba0bae5f839a84bfacec2d4c78

--- sketches/mosaic.js
+++ sketches/mosaic.js
@@ -24,7 +24,7 @@ function handleFile2(file) {
   Cells = createQuadrille(pal);
   pg = createGraphics(SAMPLE_RES * Cells.width, SAMPLE_RES);
   mosaic.setUniform('cols', Cells.width);
-  samplep();
+  mododesortp();
 }
 
 function handleFile(file) {
@@ -65,15 +65,6 @@ function setup() {
   noStroke();
   shader(mosaic);
   mosaic.setUniform('avg', false);
-  /*sel = createSelect();
-  sel.position(10, 125);
-  sel.option('keys');
-  sel.option('symbols');
-  sel.selected('symbols');
-  sel.changed(() => {
-    mosaic.setUniform('debug', sel.value() === 'keys');
-    mosaic.setUniform('color_on', false);
-  });*/
   sel = createSelect();
   sel.position(10, 125);
   sel.option('Luma(default)');
@@ -81,9 +72,8 @@ function setup() {
   sel.option('distance');
   sel.selected('Luma(default)');
   sel.changed(() => {
-    sample();
+    mododesort();
   });
-  mosaic.setUniform('color_on', false);
   video_on = createCheckbox('video', false);
   video_on.style('color', 'magenta');
   video_on.changed(() => {
@@ -100,21 +90,14 @@ function setup() {
   resolution = createSlider(1, 150, 50, 1);
   resolution.position(10, 10);
   resolution.style('width', '80px');
-  /*resolution = createSlider(10, 200, SAMPLE_RES, 1);
-  resolution.position(10, 100);
-  resolution.style('width', '80px');*/
   resolution.input(() => { mosaic.setUniform('resolution', resolution.value()) });
   mosaic.setUniform('resolution', resolution.value());
   pg = createGraphics(SAMPLE_RES * imageCells.width, SAMPLE_RES);
   mosaic.setUniform('cols', imageCells.width);
-  sample();
+  mododesort();
 }
 
-function sample() {
-  if (pg.width !== SAMPLE_RES * imageCells.width) {
-    pg = createGraphics(SAMPLE_RES * imageCells.width, SAMPLE_RES);
-    mosaic.setUniform('cols', imageCells.width);
-  }
+function mododesort() {
   if(sel.value() === 'Luma(default'){
     imageCells.sort({ mode:'LUMA',ascending: true, cellLength: SAMPLE_RES });
   }else{
@@ -126,16 +109,11 @@ function sample() {
       imageCells.sort({ mode:'DISTANCE',ascending: true, cellLength: SAMPLE_RES });
       }
   }
-  //imageCells.sort({ ascending: true, cellLength: SAMPLE_RES });
   drawQuadrille(imageCells, { graphics: pg, cellLength: SAMPLE_RES, outlineWeight: 0 });
   mosaic.setUniform('palette', pg);
 }
 
-function samplep() {
-  if (pg.width !== SAMPLE_RES * Cells.width) {
-    pg = createGraphics(SAMPLE_RES * Cells.width, SAMPLE_RES);
-    mosaic.setUniform('cols', Cells.width);
-  }
+function mododesortp() {
   if(sel.value() === 'Luma(default'){
     Cells.sort({ mode:'LUMA',ascending: true, cellLength: SAMPLE_RES });
   }else{
@@ -147,7 +125,6 @@ function samplep() {
       Cells.sort({ mode:'DISTANCE',ascending: true, cellLength: SAMPLE_RES });
       }
   }
-  //imageCells.sort({ ascending: true, cellLength: SAMPLE_RES });
   drawQuadrille(Cells, { graphics: pg, cellLength: SAMPLE_RES, outlineWeight: 0 });
   mosaic.setUniform('palette', pg);
 }
